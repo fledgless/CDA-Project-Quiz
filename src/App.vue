7fbc2d27fa87e8c56b99901840ca289c4cb8ec68
Cameron Cardinal <cameriancardinal@gmail.com>
2024-12-10 10:39:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+  <router-view/>
+</template>
+
+<script setup>
+  import Header from "./components/Header.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <p>Quiz</p>
+        <nav>
+            <ul>
+                <li><a href="/login">Se connecter</a></li>
+                <li><a href="/register">S'inscrire</a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+    header {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        background-color: #120D31;
+    }
+</style>
